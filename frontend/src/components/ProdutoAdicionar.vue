a89c2d23034c1d2ca0501ab15fa20fad8c38ee32
Raphaeljsx <raphaelalvoliv@gmail.com>
2023-07-31 20:04:52 -0300

    wip

--- frontend/src/components/ProdutoAdicionar.vue
+++ frontend/src/components/ProdutoAdicionar.vue
@@ -1,12 +1,70 @@
-<script setup></script>
+<script setup>
+import { ref, reactive } from 'vue'
+import { useAuthStore } from '../stores/auth'
+import { api } from '../utils/services'
+
+const store = useAuthStore()
+
+const produto = {
+  nome: ref(''),
+  preco: ref(0),
+  fotos: ref(null),
+  descricao: ref('')
+}
+
+function formatarProduto() {
+  produto.usuario_id = store.usuario.id
+}
+
+async function adicionarProduto() {
+  formatarProduto()
+  await api.post('/produto', produto).then(() => {
+    store.getUsuario_produtos(store.usuario.email)
+  })
+}
+</script>
 
 <template>
   <div>
     <form class="adicionar-produto">
       <label for="nome">Nome</label>
-      <input type="text" id="nome" name="nome" />
+      <input
+        type="text"
+        id="nome"
+        name="nome"
+        placeholder="Entre com o nome do produto"
+        v-model="produto.nome"
+      />
+      <label for="preco">Preço (R$)</label>
+      <input type="number" id="preco" name="preco" v-model="produto.preco" />
+      <label for="fotos">Fotos</label>
+      <input type="file" id="fotos" name="fotos" ref="fotos" />
+      <label for="descricao">Descrição</label>
+      <textarea
+        id="descricao"
+        name="descricao"
+        placeholder="Entre com a descrição do produto"
+        v-model="produto.descricao"
+      />
+      <input
+        class="btn"
+        type="button"
+        value="Adicionar Produto"
+        @click.prevent="adicionarProduto"
+      />
     </form>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.adicionar-produto {
+  display: grid;
+  grid-template-columns: 100px, 1fr;
+  align-items: center;
+  margin-bottom: 60px;
+}
+
+.btn {
+  grid-column: 2;
+}
+</style>
