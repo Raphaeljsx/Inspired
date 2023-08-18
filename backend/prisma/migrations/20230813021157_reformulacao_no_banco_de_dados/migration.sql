1a59c78876ccf303996cee7f24e0bdfdffddf601
Raphaeljsx <raphaelalvoliv@gmail.com>
2023-08-18 14:30:03 -0300

    Ajuste backend

--- backend/prisma/migrations/20230813021157_reformulacao_no_banco_de_dados/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `fotos` on the `produto` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE `produto` DROP COLUMN `fotos`,
-    ADD COLUMN `foto` LONGBLOB NULL;
